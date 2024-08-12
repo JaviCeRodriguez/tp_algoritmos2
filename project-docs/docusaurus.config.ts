6d60550f9aa1a72077d3830f650c84f22020ec8a
JaviCeRodriguez <javicerodriguez@gmail.com>
2024-08-12 00:53:51 -0300

    add: docs

---
+++ project-docs/docusaurus.config.ts
@@ -0,0 +1,130 @@
+import { themes as prismThemes } from "prism-react-renderer";
+import type { Config } from "@docusaurus/types";
+import type * as Preset from "@docusaurus/preset-classic";
+
+const config: Config = {
+  title: "Proyecto Grupo 7",
+  tagline: "Algoritmos 2 - Lic. en Ciencia de Datos - UNSAM",
+  favicon: "img/favicon.ico",
+
+  // Set the production url of your site here
+  url: "https://your-docusaurus-site.example.com",
+  // Set the /<baseUrl>/ pathname under which your site is served
+  // For GitHub pages deployment, it is often '/<projectName>/'
+  baseUrl: "/",
+
+  // GitHub pages deployment config.
+  // If you aren't using GitHub pages, you don't need these.
+  organizationName: "JaviCeRodriguez", // Usually your GitHub org/user name.
+  projectName: "tp_algoritmos2", // Usually your repo name.
+
+  onBrokenLinks: "throw",
+  onBrokenMarkdownLinks: "warn",
+
+  // Even if you don't use internationalization, you can use this field to set
+  // useful metadata like html lang. For example, if your site is Chinese, you
+  // may want to replace "en" with "zh-Hans".
+  i18n: {
+    defaultLocale: "es",
+    locales: ["es"],
+  },
+
+  presets: [
+    [
+      "classic",
+      {
+        docs: {
+          path: "docs",
+          routeBasePath: "/",
+          sidebarPath: "./sidebars.ts",
+        },
+        blog: false,
+        theme: {
+          customCss: "./src/css/custom.css",
+        },
+      } satisfies Preset.Options,
+    ],
+  ],
+
+  themeConfig: {
+    // Replace with your project's social card
+    image: "img/docusaurus-social-card.jpg",
+    navbar: {
+      title: "Proyecto Grupo 7",
+      logo: {
+        alt: "My Site Logo",
+        src: "img/logo.svg",
+      },
+      items: [
+        {
+          sidebarId: "intro",
+          position: "left",
+          label: "Introducción",
+          to: "/intro",
+        },
+        {
+          sidebarId: "guias",
+          position: "left",
+          label: "Guías",
+          to: "/category/-gu%C3%ADa-de-colaboraci%C3%B3n",
+        },
+        {
+          href: "https://github.com/JaviCeRodriguez/tp_algoritmos2",
+          label: "GitHub",
+          position: "right",
+        },
+      ],
+    },
+    footer: {
+      style: "dark",
+      links: [
+        {
+          title: "Docs",
+          items: [
+            {
+              label: "Guías",
+              to: "/category/-gu%C3%ADa-de-colaboraci%C3%B3n",
+            },
+          ],
+        },
+        // {
+        //   title: "Community",
+        //   items: [
+        //     {
+        //       label: "Stack Overflow",
+        //       href: "https://stackoverflow.com/questions/tagged/docusaurus",
+        //     },
+        //     {
+        //       label: "Discord",
+        //       href: "https://discordapp.com/invite/docusaurus",
+        //     },
+        //     {
+        //       label: "Twitter",
+        //       href: "https://twitter.com/docusaurus",
+        //     },
+        //   ],
+        // },
+        // {
+        //   title: "More",
+        //   items: [
+        //     {
+        //       label: "Blog",
+        //       to: "/blog",
+        //     },
+        //     {
+        //       label: "GitHub",
+        //       href: "https://github.com/facebook/docusaurus",
+        //     },
+        //   ],
+        // },
+      ],
+      copyright: `Copyright © ${new Date().getFullYear()} Grupo 7 de Algortimos 2 (2do cuatrimestre 2024) - LCD - UNSAM`,
+    },
+    prism: {
+      theme: prismThemes.github,
+      darkTheme: prismThemes.dracula,
+    },
+  } satisfies Preset.ThemeConfig,
+};
+
+export default config;
